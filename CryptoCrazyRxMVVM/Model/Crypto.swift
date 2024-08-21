07c018d2da5f82c8f6adb883b5750864a1f258d3
Mark Santoro <mark_santoro@comcast.net>
2024-08-21 11:38:52 -0700

    add new groups, add crypto model

---
+++ CryptoCrazyRxMVVM/Model/Crypto.swift
@@ -0,0 +1,16 @@
+//
+//  Crypto.swift
+//  CryptoCrazyRxMVVM
+//
+//  Created by Mark Santoro on 8/21/24.
+//
+
+import Foundation
+
+struct Crypto : Decodable {
+    let currency : String
+    let price : String
+    
+}
+
+
