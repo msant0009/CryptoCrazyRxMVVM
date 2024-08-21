11830fe3fc02933ba7190048fcb7f651345763c3
Mark Santoro <mark_santoro@comcast.net>
2024-08-21 08:58:48 -0700

    initial setup

--- CryptoCrazyRxMVVM/ViewController.swift
+++ CryptoCrazyRxMVVM/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        var content = cell.defaultContentConfiguration()
+        content.text = "Crypto Price"
+        content.secondaryText = "Crypto Price"
+        cell.contentConfiguration = content
+        return cell
+        
+    }
 
 }
 
